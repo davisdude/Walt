1e086881a2b9d56deecf3db21eeb588dcfeaae43
Davis <davisclaib@gmail.com>
2015-06-08 14:15:56 -0400

    getWidth and friends
    0.1.0
    ====
    - Added: Error checking for all functions now.
    - Adddd: getWidth, getHeight, and getDimensions
    - Removed: Strings of numbers are now no longer acceptable.
    - Changed: Errors are now displated "Animation error: ..." instead of
    "Animation Error"
    - Fixed: Bug not allowing variation of newAnimation where second
    argument was number.
    - Fixed: Bug where variable 'pauseAtEnd' was being replaced with
    identically named function.

--- Examples/main.lua
+++ Examples/main.lua
@@ -1,71 +1,33 @@
-require 'Utilities.Globals'
+local animator = require 'animator'
+local anim
 
-local animator = require 'utilities.animator'
-local one = love.graphics.newImage( 'Source/Images/1.png' )
-local quad = love.graphics.newImage( 'Source/Images/2 and 4.png' )
-local three = love.graphics.newImage( 'Source/Images/3.png' )
-local two = love.graphics.newQuad( 0, 0, 32, 32, 64, 32 )
-local four = love.graphics.newQuad( 32, 0, 32, 32, 64, 32 )
-local anim = animator.newAnimation( { one, two, three, four }, { 1, 1, '1', '1' }, quad )
-
--- {{{ love.load
 function love.load()
---    GameState:gotoState( 'Loading' )
-
-    input = Input()
-    input:bind( 'escape', function() love.event.quit() end )
-    camera = Camera( 0, 0, Globals.screenWidth, Globals.screenHeight )
+    love.graphics.setDefaultFilter( 'nearest', 'nearest' )
+    local one = love.graphics.newImage( 'Images/1.png' )
+    local quad = love.graphics.newImage( 'Images/2 and 4.png' )
+    local three = love.graphics.newImage( 'Images/3.png' )
+    local two = love.graphics.newQuad( 0, 0, 32, 32, 64, 32 )
+    local four = love.graphics.newQuad( 32, 0, 32, 32, 64, 32 )
+    anim = animator.newAnimation( { one, two, three, four }, { 1, 1, 1, 1 }, quad )
+    anim:setLooping()
+    rotation = 0
+    scale = 0
 end
--- }}}
 
--- {{{ love.update
 function love.update( dt )
- --   GameState:update( dt )
+    rotation = rotation + dt
+    scale = scale + dt
     anim:update( dt )
-
-    -- Input
-    input:update( dt )
 end
--- }}}
 
--- {{{ love.draw
 function love.draw()
-    camera:push()
-        anim:draw()
-  --      GameState:draw()
-    camera:pop()
-end
--- }}}
-
--- {{{ Others
-function love.keypressed( key )
-    input:keypressed( key )
+    anim:draw( 400, 300, rotation, scale, scale, anim:getWidth() / 2, anim:getHeight() / 2, scale / 10 )
 end
 
 function love.keyreleased( key )
-    input:keyreleased( key )
+    if key == 'escape' then 
+        love.event.quit()
+    else 
+        anim:restart()
+    end
 end
-
-function love.mousepressed( x, y, button )
-    input:mousepressed( button)
-end
-
-function love.mousereleased(x, y, button)
-    input:mousereleased( button )
-end
-
-function love.gamepadpressed( joystick, button )
-    input:gamepadpressed( joystick, button )
-end
-
-function love.gamepadreleased( joystick, button )
-    input:gamepadreleased( joystick, button )
-end
-
-function love.gamepadaxis( joystick, axis, value )
-    input:gamepadaxis( joystick, axis, value )
-end
-
-function love.quit()
-end
--- }}}
